afe7c4ae13b90e3a40a1d0fda15278f013a8a2a7
Kasun Lakshitha <128232048+Kasunlakshitha998@users.noreply.github.com>
2025-09-24 21:01:50 +0530

    validate sup route

--- backend/routes/suproute.js
+++ backend/routes/suproute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const suproute = express.Router();
 const supOrdController = require('../controllers/supOrdController')
+const verifyRole = require('../middleware/auth').verifyRole;
 
 suproute.get('/supOrders',supOrdController.getOrders);
-suproute.post('/createsupOrder',supOrdController.addsupOrder);
-suproute.post('/updatesupOrder',supOrdController.updatesupOrder);
-suproute.post('/deletesupOrder',supOrdController.deletesupOrder);
+suproute.post('/createsupOrder',verifyRole(['admin', 'user']), supOrdController.addsupOrder);
+suproute.post('/updatesupOrder',verifyRole(['admin', 'user']), supOrdController.updatesupOrder);
+suproute.post('/deletesupOrder',verifyRole(['admin', 'user']), supOrdController.deletesupOrder);
 
 module.exports = suproute;
