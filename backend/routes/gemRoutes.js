ce7e71600168b8441937fc244720e200170d5d4f
Kasun Lakshitha <128232048+Kasunlakshitha998@users.noreply.github.com>
2025-09-24 21:00:49 +0530

    validate gem route

--- backend/routes/gemRoutes.js
+++ backend/routes/gemRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');  
 const router = express.Router();
 const controller = require('../controllers/gemController');
-
+const verifyRole = require('../middleware/auth').verifyRole;
 
 router.get('/gems', controller.getGem);
-router.post('/addGem', controller.addGem);
-router.post('/updateGem', controller.updateGem);
-router.post('/deleteGem', controller.deleteGem);
+router.post('/addGem',verifyRole(['admin']), controller.addGem);
+router.post('/updateGem', verifyRole(['admin']), controller.updateGem);
+router.post('/deleteGem', verifyRole(['admin']), controller.deleteGem);
 
 
 module.exports = router;
