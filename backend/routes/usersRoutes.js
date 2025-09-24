b9e232521d80307e8b91170376419e4eb7b275a6
Kasun Lakshitha <128232048+Kasunlakshitha998@users.noreply.github.com>
2025-09-24 21:02:50 +0530

    update

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/UsersController');
+const verifyRole = require('../middleware/auth').verifyRole;
 
 router.get('/users', controller.getUsers);
-router.post('/createuser', controller.addUser);
-router.post('/updateuser', controller.updateUser);
-router.post('/deleteuser', controller.deleteUser);
+router.post('/createuser', verifyRole(['admin', 'user']), controller.addUser);
+router.post('/updateuser', verifyRole(['admin', 'user']), controller.updateUser);
+router.post('/deleteuser', verifyRole(['admin', 'user']), controller.deleteUser);
 
-router.post('/update-password', controller.changepassword);
+router.post('/update-password', verifyRole(['admin', 'user']), controller.changepassword);
 
 module.exports =router;
