2df2264428cbfea0c05ec62d4c7608a95553d9f0
Kasun Lakshitha <128232048+Kasunlakshitha998@users.noreply.github.com>
2025-09-24 21:03:29 +0530

    validate order route

--- backend/routes/ordersRoutes.js
+++ backend/routes/ordersRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/OrderController');
+const verifyRole = require('../middleware/auth').verifyRole;
 
-router.get('/orders', controller.getOrders);
-router.post('/createorder', controller.addOrder);
-router.post('/updateorder', controller.updateOrder);
-router.post('/deleteorder', controller.deleteOrder);
+router.get('/orders', verifyRole(['admin']), controller.getOrders);
+router.post('/createorder', verifyRole(['admin', 'user']), controller.addOrder);
+router.post('/updateorder', verifyRole(['admin']), controller.updateOrder);
+router.post('/deleteorder', verifyRole(['admin']), controller.deleteOrder);
 
 module.exports =router;
