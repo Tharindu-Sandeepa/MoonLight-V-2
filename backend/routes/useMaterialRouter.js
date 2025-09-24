73634c6511f1c902482e58679b2a17463608b714
Kasun Lakshitha <128232048+Kasunlakshitha998@users.noreply.github.com>
2025-09-24 21:02:16 +0530

    validate userMaterial route

--- backend/routes/useMaterialRouter.js
+++ backend/routes/useMaterialRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const useController = require('../controllers/useMaterialController');
+const verifyRole = require('../middleware/auth').verifyRole;
 
 router.get('/usematerials' , useController.getusemat);
-router.post('/addusematerial' , useController.addusemat);
-router.post('/updateusematerial' , useController.updateusemat);
-router.post('/deleteusematerial' , useController.deleteusemat);
+router.post('/addusematerial' , verifyRole(['admin', 'user']), useController.addusemat);
+router.post('/updateusematerial' , verifyRole(['admin', 'user']), useController.updateusemat);
+router.post('/deleteusematerial' , verifyRole(['admin', 'user']), useController.deleteusemat);
 
 module.exports= router;
