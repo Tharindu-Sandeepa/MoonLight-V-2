dd960fae6f9ef6b5cd3abf693b6b7cc92d76cfcf
Kasun Lakshitha <128232048+Kasunlakshitha998@users.noreply.github.com>
2025-09-24 21:00:32 +0530

    validate feedback route

--- backend/routes/feedbackRouter.js
+++ backend/routes/feedbackRouter.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/feedbackController');
+const verifyRole = require('../middleware/auth').verifyRole;
+
 
 router.get('/feedbacks',controller.getFeedback);
-router.post('/createfeedback',controller.addFeedback);
-router.post('/updatefeedback',controller.updateFeedback);
-router.post('/deletefeedback',controller.deleteFeedback);
+router.post('/createfeedback', verifyRole(['admin', 'user']), controller.addFeedback);
+router.post('/updatefeedback', verifyRole(['admin', 'user']), controller.updateFeedback);
+router.post('/deletefeedback',verifyRole(['admin', 'user']), controller.deleteFeedback);
 
 module.exports = router;
