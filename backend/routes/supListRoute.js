4066eafbbc0aac709156dfce91cb51bae0a7ec78
Kasun Lakshitha <128232048+Kasunlakshitha998@users.noreply.github.com>
2025-09-24 21:01:31 +0530

    validate supList route

--- backend/routes/supListRoute.js
+++ backend/routes/supListRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const supListRoute = express.Router();
 const supListController = require('../controllers/supListController')
+const verifyRole = require('../middleware/auth').verifyRole;
 
 supListRoute.get('/supList',supListController.getSupplier);
-supListRoute.post('/createsupplier',supListController.addSupplier);
-supListRoute.post('/updatesupplier',supListController.updateSupplier);
-supListRoute.post('/deletesupplier',supListController.deleteSupplier);
+supListRoute.post('/createsupplier', verifyRole(['admin', 'user']), supListController.addSupplier);
+supListRoute.post('/updatesupplier',verifyRole(['admin', 'user']), supListController.updateSupplier);
+supListRoute.post('/deletesupplier',verifyRole(['admin', 'user']), supListController.deleteSupplier);
 
 module.exports = supListRoute;
